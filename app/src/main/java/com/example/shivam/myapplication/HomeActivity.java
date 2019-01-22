9c3c60f7334fc9862b9cbbae11da321049dab12b
Shivam <shng8750@gmail.com>
2019-01-22 14:53:45 +0530

    feat: adding the remaining files

--- app/src/main/java/com/example/shivam/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/shivam/myapplication/HomeActivity.java
@@ -58,9 +58,10 @@ public class HomeActivity extends AppCompatActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view)
+            {
+               Intent intent = new Intent(HomeActivity.this,CartActivity.class);
+               startActivity(intent);
             }
         });
 
@@ -176,7 +177,8 @@ public class HomeActivity extends AppCompatActivity
 
         if (id == R.id.nav_cart)
         {
-            // Handle the camera action
+            Intent intent = new Intent(HomeActivity.this,CartActivity.class);
+            startActivity(intent);
         }
         else if (id == R.id.nav_orders)
         {
