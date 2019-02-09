1f8f0d5256cd8a1413df8ec4f91731495b59e006
Shivam <shng8750@gmail.com>
2019-02-09 12:35:06 +0530

    feat: adding the remaining files

--- app/src/main/java/com/example/shivam/myapplication/SearchProductsActivity.java
+++ app/src/main/java/com/example/shivam/myapplication/SearchProductsActivity.java
@@ -1,17 +1,31 @@
 package com.example.shivam.myapplication;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.shivam.myapplication.Model.Products;
+import com.example.shivam.myapplication.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class SearchProductsActivity extends AppCompatActivity {
 
     private Button searchBtn ;
     private EditText inputText ;
     private RecyclerView searchList ;
+    private String SearchInput ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +38,60 @@ public class SearchProductsActivity extends AppCompatActivity {
 
         searchList.setLayoutManager(new LinearLayoutManager(SearchProductsActivity.this));
 
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+               SearchInput = inputText.getText().toString();
 
+               onStart();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>()
+                         .setQuery(reference.orderByChild("pname").startAt(SearchInput),Products.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model)
+                    {
+                        holder.txtProductName.setText(model.getPname());
+                        holder.txtProductDescription.setText(model.getDescription());
+                        holder.txtProductPrice.setText("Price =" + model.getPrice() + " $ ");
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v)
+                            {
+                                Intent intent = new Intent(SearchProductsActivity.this,ProductDetailsActivity.class);
+                                intent.putExtra("pid",model.getPid());
+                                startActivity(intent);
+                            }
+                        });
+                    }
+
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i)
+                    {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout,parent,false);
+                        ProductViewHolder holder = new ProductViewHolder(view);
+                        return holder;
+                    }
+                } ;
+        searchList.setAdapter(adapter);
+        adapter.startListening();
     }
 }
